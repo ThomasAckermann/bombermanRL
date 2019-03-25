3aa989fb46d0501504d8b6d7a7090b6360915de1
Carl <Carl@Carls-MBP.local>
2019-03-25 23:17:20 +0100

    more training with opponents
    (and some other changes)

---
+++ copy_bm_3_alt/agent_code/qn_agent_alt/create_data.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+np.save('q_data.npy', np.array([np.r_[-1, np.zeros(32)]]))  # ,(16,1)))
+np.save('all_data.npy', np.array([np.r_[-1, np.zeros(32)]]))  # ,(16,1)))
+
+
+# np.save('theta_q.npy', np.zeros((6, 32)))
+np.save('y_data.npy', np.array([0]))
+
+theta_alt = np.load('theta_q.npy')
+print(np.shape(theta_alt), theta_alt)
+tmp = theta_alt[:, -1]
+tmp = np.reshape(tmp, (6, 1))
+theta_alt = np.append(theta_alt[:,:-1], np.zeros((6, 4)), axis = -1)
+theta_alt = np.append(theta_alt, tmp, axis = -1)
+print(np.shape(theta_alt), theta_alt)
+# np.save('theta_q.npy', theta_alt)
+
+
