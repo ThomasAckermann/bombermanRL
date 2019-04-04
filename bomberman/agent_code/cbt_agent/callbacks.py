45ab44350471364649ff0f65ac8aad6358a07ed6
Carl <Carl@Carls-MBP.local>
2019-04-04 17:47:15 +0200

    removed training code from callbacks

--- bomberman/agent_code/cbt_agent/callbacks.py
+++ bomberman/agent_code/cbt_agent/callbacks.py
@@ -1,15 +1,6 @@
 import numpy as np
-from time import sleep
 from scipy.optimize import curve_fit
-import sys 
-import traceback
-import random
-import copy
 
-moves = np.array([[]])
-path = '' #'./agent_code/cbt_agent/'
-crate_counter = 0
-round_number = 1
 
 def func_curve(X, a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_10, a_11, a_12, a_13, a_14, a_15, a_16, a_17, a_18, a_19, a_20, a_21, a_22, a_23, a_24, a_25, a_26, a_27, a_28, a_29, a_30, a_31, a_32, a_33, a_34):
     (x_1, x_2, x_3, x_4, x_5, x_6, x_7, x_8, x_9, x_10, x_11, x_12, x_13, x_14, x_15, x_16, x_17, x_18, x_19, x_20, x_21, x_22, x_23, x_24, x_25, x_26, x_27, x_28, x_29, x_30, x_31, x_32, x_33) = X
@@ -305,8 +296,6 @@ def setup(self):
 ,-2.17640318e+00,-4.78100476e-01,-9.28862489e-01,-3.35639105e+01
 ,5.25775985e+00,-8.19891458e-01]])
     self.last_coords = [0, 0]
-    #self.theta = np.load('{}thetas/theta_q.npy'.format(path))
-
 
 def act(self):
     self.next_action = ['LEFT', 'RIGHT', 'UP', 'DOWN', 'WAIT', 'BOMB'][int(np.argmax(q_function(self.theta, build_features(self))))]
@@ -314,142 +303,6 @@ def act(self):
 
 def reward_update(self):
     return None
-    global moves, round_number
-    last_event = self.q_data[-1]
-    f = last_event[2:]
-    
-    #sleep(10)
-
-    event_dict = {0 : "MOVED_LEFT",
-            1 : "MOVED_RIGHT",
-            2 : "MOVED_UP",
-            3 : "MOVED_DOWN",
-            4 : "WAITED",
-            5 : "INTERRUPTED",
-            6 : "INVALID_ACTION",
-            7 : "BOMB_DROPPED",
-            8 : "BOMB_EXPLODED",
-            9 : "CRATE_DESTROYED",
-            10 : "COIN_FOUND",
-            11 : "COIN_COLLECTED",
-            12 : "KILLED_OPPONENT",
-            13 : "KILLED_SELF",
-            14 : "GOT_KILLED",
-            15 : "OPPONENT_ELIMINATED",
-            16 : "SURVIVED_ROUND"}
-
-    rewards = {"MOVED_LEFT" : -2 + f[1] + 0.2*f[3] + 0.2*f[5] - 3*f[7] - 35*f[18] + 2*f[22] + 0.4*f[24] - 22*f[27], 
-                    "MOVED_RIGHT" : -2 - f[1] - 0.2*f[3] + 0.2*f[5] - 3*f[8] - 35*f[19] - 2*f[22] - 0.4*f[24] - 22*f[28],
-                    "MOVED_UP" : -2 + f[2] + 0.2*f[4] + 0.2*f[6] - 3*f[9] - 35*f[20] + 2*f[23] + 0.4*f[25] - 22*f[29],
-                    "MOVED_DOWN" : -2 - f[2] - 0.2*f[4] + 0.2*f[6] - 3*f[10] - 35*f[21] - 2*f[23] - 0.4*f[25] - 22*f[30],
-                    "WAITED" : -6 - 300*f[11], 
-                    "INTERRUPTED" : 0,
-                    "INVALID_ACTION" : -5 - 5*f[5] - 5*f[6] - 50*f[12] - 10*np.clip(np.sum(f[14:18]), 0, 1),
-                    
-                    "BOMB_DROPPED" :  -5 + 5*f[13] + 2*f[26] - 30*f[31] + 10*f[32] - 4*f[11],
-                    "BOMB_EXPLODED" : 0, 
-                    
-                    "CRATE_DESTROYED" : 2,
-                    "COIN_FOUND" : 0,
-                    "COIN_COLLECTED" : 20,
-
-                    "KILLED_OPPONENT" : 0,
-                    "KILLED_SELF" : 0,
-
-                    "GOT_KILLED" : 0,
-                    "OPPONENT_ELIMINATED" : 0,
-                    "SURVIVED_ROUND" : 0}
-
-    reward = 0.667761 * np.sum([rewards[event_dict[item]] for item in self.events])
-
-    global crate_counter
-    for event in self.events:
-        if event == 9:
-            crate_counter += 1
-
-    moves = np.append(moves, [last_event[0], reward, *f])
-    moves = np.reshape(moves, (int(len(moves.flat)/(2+len(f))), 2+len(f)))
-    return None
-
 
 def end_of_episode(self):
     return None
-    try:
-        global moves, round_number, path, crate_counter
-        alpha = 0.04        
-        gamma = 0.4
-        n = 7    
-        for t in range(len(moves)):
-            if (t >= len(moves) - n):
-                n = len(moves) - t - 1
-            q_next = np.max(q_function(self.theta, moves[t + n, 2:])) 
-            y_t = np.sum([gamma**(t_ - t - 1) * moves[t_, 1] for t_ in range(t + 1, t + n + 1)]) + gamma**n * q_next
-
-            q_update = self.q_data[-len(moves) + t, 1] + alpha * (y_t - self.q_data[-len(moves) + t, 1])   
-            
-            self.q_data[-len(moves) + t, 1] = q_update        
-
-        
-        with open('{}moves.txt'.format(path), 'a') as f:
-            new_coins = self.game_state['self'][4]
-            last_score = self.game_state['self'][4]
-            f.write(str(round_number) + ' ' + str(len(moves)) + ' ' + str(new_coins) + ' ' + str(crate_counter) + '\n')
-            crate_counter = 0
-
-        if (round_number % 100 == 0):
-            try: 
-                theta = []
-                for i in range(6):
-                    regression_data = copy.deepcopy(self.q_data)
-                    if len(self.all_data) > 6000:
-                        tmp = self.all_data[:-6000]
-                        if len(tmp) > 6000:
-                            tmp = tmp[np.array(random.sample(list(np.arange(len(tmp))), 6000))]
-                        regression_data = np.append(tmp, regression_data, axis = 0)
-                    
-                    mask = regression_data[:,0]==i
-                    regression_data = regression_data[mask][:, 1:]
-                    
-                    popt, pcov = curve_fit(func_curve, (regression_data[:, 1:].T), regression_data[:, 0], p0=self.theta[i])
-                    theta.append(popt)
-                self.theta = np.clip(np.array(theta), -90, 20)
-
-            except Exception as e:
-                print(e)
-                print('theta unverändert')
-                print('Exception as e:')
-                print('line: ', sys.exc_info()[2].tb_lineno)
-                print(type(e), e) 
-                print('Traceback:')
-                traceback.print_tb(sys.exc_info()[2], file = sys.stdout)
-                print('end of traceback.')
-                pass
-         
-        if len(self.q_data) > 6000:
-            self.q_data = self.q_data[-6000:]       
-        
-        if len(self.all_data) > 30000:
-            self.all_data = self.all_data[np.array(random.sample(list(np.arange(len(self.all_data))), 30000))]    
-
-        if round_number % 20 == 0:
-            np.save('{}thetas/theta_q.npy'.format(path), self.theta)
-            np.save('{}q_data/q_data.npy'.format(path), self.q_data)
-            np.save('{}all_data/all_data.npy'.format(path), self.all_data)
-            np.save('{}thetas/theta_nach_{}_spielen.npy'.format(path, round_number), self.theta)
-            if round_number % 60 == 0:
-                np.save('{}all_data/all_data_nach_{}_spielen.npy'.format(path, round_number), self.all_data)
-
-        round_number += 1
-        moves = np.array([[]])    
-        # print('END')
-    except Exception as e:
-        print('Exception as e:')
-        print('line: ', sys.exc_info()[2].tb_lineno)
-        print(type(e), e) 
-        print('Traceback:')
-        traceback.print_tb(sys.exc_info()[2], file = sys.stdout)
-        print('end of traceback.')
-        
-    return None
-
-
